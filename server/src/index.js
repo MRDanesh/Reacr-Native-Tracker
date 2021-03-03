76b989426db8c5a79cbcfb5a55367db712f9445b
MRDanesh <Mohammadrezad126@gmail.com>
2021-03-03 10:27:25 -0500

    creating track in server setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,19 @@
 require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
+
 app.use(bodyParser.json());
 
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri = 'mongodb+srv://MRDaneshTracker:eEMHm4JM6iDX2ICt@tracker-cluster.fzgqx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.connect(mongoUri, {
@@ -25,6 +29,7 @@ mongoose.connection.on('connected', () => {
 mongoose.connection.on('error', (err) => {
     console.log('Error in mongo database conncetion', err);
 });
+
 
 app.get('/', requireAuth, (req, res) => {
     res.send(`Your email is: ${req.user.email}`);
